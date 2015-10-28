625ee5a975b81de29e4a6cbfd73e136ddeacdc72
Tori Brenner <vmbrenner@email.arizona.edu>
2015-10-28 11:19:07 -0700

    gave some pages more html/css

---
+++ error.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>Error!</title>
+	</head>
+
+	<body>
+		<div id="outerlogin">
+		<div id="login">
+		<div id="motto"><br />something went wrong with your login!
+		<h5><a href="newuser.php">click here to try again</a></h5></div></div></div>
+	</body>
+</html>
