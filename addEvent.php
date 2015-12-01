500071f6887f20f30987b652841fc69c50e6adbc
System Administrator <robertcmacdonald@email.arizona.edu>
2015-11-30 17:48:16 -0700

    Tweaked team page css

--- addEvent.php
+++ addEvent.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 require_once("teamFunctions.php");
 verifyLogin();
 
-if(!isset($_GET["teamName"]) || empty($_POST["eventName"]) || empty($_POST["daysUntilEvent"]) || empty($_POST["eventDesc"]) ){
+if(!isset($_GET["teamName"]) || empty($_POST["eventName"]) || empty($_POST["dateOfEvent"]) || empty($_POST["eventDesc"]) ){
     echo "Must fill out all fields\n";
     echo "<script>setTimeout(\"window.location='profile.php'\", 3000);</script>";
     exit();
@@ -23,15 +23,17 @@ try{
     #Get the current date
     $currentDate = date("Y-m-d");
 
+    /*
     #Calculate the due date
     $timeOf = mktime(0, 0, 0, date("m")  , date("d")+$_POST["daysUntilEvent"], date("Y"));
     $dateOf = date("Y-m-d", $timeOf);
+    */
 
     #Get the creator
     $creator = $_SESSION["username"];
 
     #Insert the task information into the team database. Person responsible will be set later.
-    addEvent($teamName, $_POST["eventName"], $_POST["eventDesc"], $currentDate, $dateOf, $creator);
+    addEvent($teamName, $_POST["eventName"], $_POST["eventDesc"], $currentDate, $_POST['dateOfEvent'], $creator);
 
     header("Location: teamPage.php?&teamName=$teamName");
 
--- teamPage.php
+++ teamPage.php
@@ -1,94 +1,103 @@
 <?php
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
-	require_once("teamFunctions.php");
-	verifyLogin();
-	
-	if(!isset($_GET["teamName"]) || !verifyMembership($_GET["teamName"])){	
-		echo "<script>setTimeout(\"window.location='error.php'\", 100);</script>";
-		#echo "Invalid team\n";
-		#echo "<script>setTimeout(\"window.location='profile.php'\", 3000);</script>";
-		exit();
-	}
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once("teamFunctions.php");
+verifyLogin();
+
+if(!isset($_GET["teamName"]) || !verifyMembership($_GET["teamName"])){
+    echo "<script>setTimeout(\"window.location='error.php'\", 100);</script>";
+    #echo "Invalid team\n";
+    #echo "<script>setTimeout(\"window.location='profile.php'\", 3000);</script>";
+    exit();
+}
 ?>
 
 <!doctype html>
 <html>
-	<head>
-		<title><?php echo $_GET["teamName"]; ?></title>
-		<link rel="stylesheet" type="text/css" href="style.css">
-	</head>
-	
-	<body id="teamPageBody">
-		<div id="header"><?php echo $_GET["teamName"]; ?></div>
-		<div id="navteam"><a href="index.html">home</a><br/>
-		<a href="profile.php">user panel</a></br>
-		<a href="<?php echo 'taskPage.php?&teamName='.$_GET['teamName'] ?>">tasks</a><br />
-		<a href="login.php">login</a></div>
-		<div id="teamPageOut" align="center">
-		<div id="teamPageForms">
-		<!-- <a href="<?php echo 'taskPage.php?&teamName='.$_GET['teamName'] ?>">View team tasks</a> -->
-		<!-- Kind of ugly to be embedding PHP into an HTML tag like this, but we need to pass this data to these scripts  -->
-		<form class="form" action="<?php echo 'addUserToTeam.php?&teamName='.$_GET['teamName'] ?>" method="post">
-			<table id ="addMemberTable">
-				<tr>
-					<td><label for="newMember">Add new member</label></td>
-					<td><input type="text" name="newMember" placeholder="New Member"/></td>
-				</tr>
-			</table>
-			<input type="submit"/></br>
-		</form>
-		</br>
-		<form class="form" action="<?php echo 'addTask.php?&teamName='.$_GET['teamName'] ?>" method="post">
-			<table id="createTaskTable">
-				<tr>
-					<td><label for="taskName">Task Name</label></td>
-					<td><input type="text" name="taskName" placeholder="Task Name" maxlength="100"/></td>
-				</tr>
-				<tr>
-					<td><label for="daysToComplete">Days to Complete</label></td>
-					<td><input type="number" name="daysToComplete" min="1" value="1"/></td>
-				</tr>
-				<tr>
-					<td><label for="taskDesc">Task Description</label></td>
-					<td><textarea name="taskDesc" maxlength="3000" cols=50 rows=6></textarea></td>
-				</tr>
-			</table>
-			<input type="submit"/></br>
+<head>
+    <title><?php echo $_GET["teamName"]; ?></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body id="teamPageBody">
+<div id="header"><?php echo $_GET["teamName"]; ?></div>
+<div id="navteam"><a href="index.html">home</a><br/>
+    <a href="profile.php">user panel</a></br>
+    <a href="<?php echo 'taskPage.php?&teamName='.$_GET['teamName'] ?>">tasks</a><br />
+    <a href="login.php">login</a></div>
+<div id="teamPageOut" align="center">
+    <div id="teamPageForms">
 
-		</form>
-		<form class="form" action="<?php echo 'addEvent.php?&teamName='.$_GET['teamName'] ?>" method="post">
-			<table id="createEventTable">
-				<tr>
-					<td><label for="eventName">Event Name</label></td>
-					<td><input type="text" name="eventName" placeholder="Event Name" maxlength="100"/></td>
-				</tr>
-				<tr>
-					<!-- TODO: Add calendar for selecting date of event -->
-					<td><label for="daysUntilEvent">Days to Complete</label></td>
-					<td><input type="number" name="daysUntilEvent" min="1" value="1"/></td>
-				</tr>
-				<tr>
-					<td><label for="eventDesc">Event Description</label></td>
-					<td><textarea name="eventDesc" maxlength="3000" cols=50 rows=6></textarea></td>
-				</tr>
-			</table>
-			<input type="submit"/></br>
+        <form action="<?php echo 'getTeamTasksForUser.php?&teamName=' . $_GET['teamName'] ?>" method="post">
+            <input type="hidden" name="taskUser" value="<?php echo $_SESSION['username']?>"/>
+            <input type="submit" value="View my tasks for this team!"/>
+        </form>
+        </br>
 
-		</form>
-		</div></div>
-		</br>
-		<form action="getTeamTasksForUser.php" method="get">
-			<input type="hidden" name="username" value="<?php echo $_SESSION['username']?>"/>
-			<input type="hidden" name="teamName" value="<?php echo $_GET['teamName']   ?>"/>
-			<input type="submit" value="View my tasks"/>
-		</form>
-		</br>
-		<form action="getTeamTasksForUser.php" method="get">
-			<input type="text" name="username" placeholder="Username" maxlength="100"/>
-			<input type="hidden" name="teamName" value="<?php echo $_GET['teamName']  ?>"/>
-			<input type="submit" value="View user's tasks"/>
-		</form>
+        <form action="<?php echo 'getTeamTasksForUser.php?&teamName=' . $_GET['teamName'] ?>" method="post">
+            <input type="text" name="taskUser" placeholder="Username" maxlength="100"/>
+            <input type="submit" value="View user's tasks"/>
+        </form>
 
-	</body>
+        <!-- <a href="<?php echo 'taskPage.php?&teamName='.$_GET['teamName'] ?>">View team tasks</a> -->
+        <!-- Kind of ugly to be embedding PHP into an HTML tag like this, but we need to pass this data to these scripts  -->
+        </br>
+        <form id="addTaskForm" class="form" action="<?php echo 'addTask.php?&teamName='.$_GET['teamName'] ?>" method="post">
+            <table id="createTaskTable">
+                <caption>Add new task</caption>
+                <tr>
+                    <td><label for="taskName">Task name</label></td>
+                    <td><input type="text" name="taskName" placeholder="Task name" maxlength="100"/></td>
+                </tr>
+                <tr>
+                    <td><label for="daysToComplete">Days to complete</label></td>
+                    <td><input type="number" name="daysToComplete" min="1" value="1"/></td>
+                </tr>
+                <tr>
+                    <td><label for="taskDesc">Task description</label></td>
+                    <td><textarea name="taskDesc" maxlength="3000" cols=50 rows=6></textarea></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit"/></br></td>
+                </tr>
+            </table>
+        </form>
+        <form class="form" action="<?php echo 'addEvent.php?&teamName='.$_GET['teamName'] ?>" method="post">
+            <table id="createEventTable">
+                <caption>Add new event</caption>
+                <tr>
+                    <td><label for="eventName">Event name</label></td>
+                    <td><input type="text" name="eventName" placeholder="Event name" maxlength="100"/></td>
+                </tr>
+                <tr>
+                    <!-- TODO: Add calendar for selecting date of event -->
+                    <td><label for="dateOfEvent">Date of event</label></td>
+                    <td><input type="date" name="dateOfEvent" required/></td>
+                </tr>
+                <tr>
+                    <td><label for="eventDesc">Event description</label></td>
+                    <td><textarea name="eventDesc" maxlength="3000" cols=50 rows=6></textarea></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit"/></br></td>
+                </tr>
+            </table>
+        </form>
+        </br>
+        <form class="form" action="<?php echo 'addUserToTeam.php?&teamName='.$_GET['teamName'] ?>" method="post">
+            <table id ="addMemberTable">
+                <tr>
+                    <td><label for="newMember">Add new member</label></td>
+                    <td><input type="text" name="newMember" placeholder="New member"/></td>
+                </tr>
+            </table>
+            <input type="submit"/></br>
+        </form>
+    </div></div>
+</br>
+<script>
+    // NYI
+</script>
+</body>
 </html>
